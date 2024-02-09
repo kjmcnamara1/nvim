277deb4bf3f01f81502f509474b8b69fb1068eca
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-09 00:24:31 -0600

    comment out ufo

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -1,10 +1,10 @@
 return {
-  "kevinhwang91/nvim-ufo",
-  event = 'BufEnter',
-  dependencies = "kevinhwang91/promise-async",
-  opts = {
-    provider_selector = function(_bufnr, _filetype, _buftype)
-      return {'treesitter','indent'}
-    end,
-  }
+--   "kevinhwang91/nvim-ufo",
+--   event = 'BufEnter',
+--   dependencies = "kevinhwang91/promise-async",
+--   opts = {
+--     provider_selector = function(_bufnr, _filetype, _buftype)
+--       return {'treesitter','indent'}
+--     end,
+--   }
 }
