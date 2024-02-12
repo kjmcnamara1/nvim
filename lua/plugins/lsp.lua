756c53b72f827a16de583d21e4e2f15d45fed8ba
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-02-11 19:46:16 -0600

    configure diagnostic signs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,6 +38,17 @@ return {
         ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
       }
 
+      -- Customize diagnostics
+      vim.diagnostic.config({
+        virtual_text = { source = "always" }
+      })
+      -- Change the Diagnostic symbols in the sign column (gutter)
+      local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end
+
       require("mason-lspconfig").setup_handlers({
         function(ls)
           require("lspconfig")[ls].setup({
