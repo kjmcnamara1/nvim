6cd7d6f3b41d6a7ed9b7f4a7d1ced5efaf7a390e
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-07-16 14:15:14 -0400

    reconfigure whichkey for new version

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -3,7 +3,9 @@ return {
   "folke/which-key.nvim",
   cond = not vim.g.vscode,
   event = "VeryLazy",
-  cmd = "WhichKey",
+  -- cmd = "WhichKey",
+  keys = { { "<leader>?", function() require("which-key").show() end, desc = "Buffer Local Keymaps (which-key)" } },
+  opts = { preset = "helix" },
   -- init = function()
   --   vim.o.timeout=true
   --   vim.o.timeoutlen=300
