f564ff2a7e01daa448556e1e663abbe39a4019f0
Kevin J. McNamara <kjmcnamara1@gmail.com>
2024-07-28 17:24:13 -0400

    add kitty-navigator plugin

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -96,4 +96,19 @@ return {
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
+  {
+    "MunsMan/kitty-navigator.nvim",
+    -- enabled = false,
+    cond = not vim.g.vscode,
+    build = {
+      "cp navigate_kitty.py ~/.config/kitty",
+      "cp pass_keys.py ~/.config/kitty",
+    },
+    keys = {
+      { "<C-h>", function() require("kitty-navigator").navigateLeft() end,  desc = "Move left a Split",  mode = { "n" } },
+      { "<C-j>", function() require("kitty-navigator").navigateDown() end,  desc = "Move down a Split",  mode = { "n" } },
+      { "<C-k>", function() require("kitty-navigator").navigateUp() end,    desc = "Move up a Split",    mode = { "n" } },
+      { "<C-l>", function() require("kitty-navigator").navigateRight() end, desc = "Move right a Split", mode = { "n" } },
+    },
+  }
 }
