72a8c7d2f1b1ae00d50ca17d22d5d1c978877084
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-01-25 12:11:09 -0500

    feat(init): clean init and add colorscheme plugin

--- init.lua
+++ init.lua
@@ -1,35 +1,14 @@
--- Vim options
-require("options")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
--- Custom keymaps
-require("keymaps")
+require("core.options")
 
--- Custom autocmds
-require("autocmds")
-
--- ===================== Lazy.nvim Plugin Manager =============================
--- Set lazy data folder location where plugins will download
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-
--- Add plugin data path to beginning of vim runtime path
-vim.opt.rtp:prepend(lazypath)
-
--- Load Lazy.nvim
-require("lazy").setup("plugins", {
-  ui = {
-    border = "rounded",
-    title = "Lazy Plugin Manager",
-  },
-  checker = { enabled = true, frequency = 86400 },
-  change_detection = { notify = false },
-})
+vim.pack.add({ "https://github.com/rmehri01/onenord.nvim" })
+require("onenord").setup(
+  {
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+    },
+  }
+)
