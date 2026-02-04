598926843eb59beb8db275d1a213d70b27e07e7f
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-02-04 18:12:11 -0500

    Refactor Neovim minimal profile configuration for clarity

--- after/plugin/minimal.lua
+++ after/plugin/minimal.lua
@@ -2,16 +2,18 @@ if profile() ~= "minimal" then
   return
 end
 
-vim.opt.laststatus     = 0 -- Turn off statusline
-vim.opt.scrolloff      = 0
-vim.opt.sidescrolloff  = 0
-vim.opt.number         = false
-vim.opt.relativenumber = false
+local o          = vim.opt
 
-vim.opt.cmdheight      = 0
-vim.opt.showmode       = false
-vim.opt.showcmd        = false
-vim.opt.shortmess:append("F")
+o.laststatus     = 0 -- Turn off statusline
+o.scrolloff      = 0
+o.sidescrolloff  = 0
+o.number         = false
+o.relativenumber = false
+
+o.cmdheight      = 0
+o.showmode       = false
+o.showcmd        = false
+o.shortmess:append("F")
 
 wk.add({
   { "<esc>", "ZQ", desc = "Quit" },
