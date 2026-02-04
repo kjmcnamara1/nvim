dcd3b733e66171610f218de883d13e21aa7bed9b
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-02-04 18:12:12 -0500

    Refine Lua ftplugin library setup for lazydev

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -2,9 +2,7 @@ vim.pack.add({ "https://github.com/folke/lazydev.nvim" })
 
 require("lazydev").setup({
   library = {
-    "nvim-dap-ui",
-    --   vim.fn.stdpath("config") .. "/lua",
-    --   { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-    --   { path = "snacks.nvim",        words = { "Snacks" } },
+    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    { path = "snacks.nvim",        words = { "Snacks" } },
   }
 })
