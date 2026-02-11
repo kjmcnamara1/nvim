53e2f05b1e01108ec03f819279f701b5793e28d7
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-02-11 02:49:31 -0500

    feat(icons): add icons for chezmoi files

--- lua/config/icons.lua
+++ lua/config/icons.lua
@@ -8,6 +8,17 @@ require("mini.icons").setup({
   file = {
     [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
     ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+    [".chezmoiignore"] = { glyph = "", hl = "MiniIconsGrey" },
+    [".chezmoiremove"] = { glyph = "", hl = "MiniIconsGrey" },
+    [".chezmoiroot"] = { glyph = "", hl = "MiniIconsGrey" },
+    [".chezmoiversion"] = { glyph = "", hl = "MiniIconsGrey" },
+    ["bash.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+    ["json.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+    ["ps1.tmpl"] = { glyph = "󰨊", hl = "MiniIconsGrey" },
+    ["sh.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+    ["toml.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+    ["yaml.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
+    ["zsh.tmpl"] = { glyph = "", hl = "MiniIconsGrey" },
   },
   filetype = {
     dotenv = { glyph = "", hl = "MiniIconsYellow" },
