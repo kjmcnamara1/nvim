684a225b0370d46fabe98f7de277dfdd8758be21
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-02-11 02:52:42 -0500

    feat(todo): customize todo comments

--- lua/config/todo.lua
+++ lua/config/todo.lua
@@ -1,5 +1,6 @@
--- [ ]: Add pattern for todo checkbox
--- [ ]: Change highlight for TODO to yellow
+-- TODO: Add pattern for todo checkbox
+-- TODO: Change highlight for TODO to yellow
+-- BUG: Highlighting is not working for checkboxes
 
 if profile() ~= "default" then
   return
@@ -8,12 +9,23 @@ end
 pack_add({ "https://github.com/folke/todo-comments.nvim" })
 
 require("todo-comments").setup({
+  highlight = {
+    -- pattern = { [[.*<(KEYWORDS)\s*:]], [[.*%[ %]\s*:]] },
+    -- pattern = { [[.*<(KEYWORDS)\s*:|%[.%]:]] },
+  },
   keywords = {
-    TODO = { color = "todo", alt = { "[ ]" } },
-    DONE = { color = "info", alt = { "[x]", "[X]" } },
+    TODO = { icon = " ", color = "todo" },
+    DONE = { icon = " ", color = "info" },
+    BUG = { icon = " ", color = "error", alt = { "FIXME", "FIX", "FIXIT", "ISSUE" } },
+    HACK = { icon = " ", color = "warning" },
+    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+    NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+    TEST = { icon = " ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
   },
   colors = {
-    todo = { "@comment.todo", "#FFFF00" },
+    todo = { "@comment.todo" },
+    test = { colors.dark_blue },
   },
 
 })
@@ -21,3 +33,6 @@ wk.add({
   { "]t", require("todo-comments").jump_next, desc = "Next todo comment" },
   { "[t", require("todo-comments").jump_prev, desc = "Previous Todo Comment" },
 })
+
+-- require("todo-comments.config").keywords
+-- require("todo-comments.config").hl_regex
