5e6457707f86a6fb75f74f7bd951a312b5817063
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-02-11 02:43:29 -0500

    fix(vscode): fix vscode keymaps

--- after/plugin/vscode.lua
+++ after/plugin/vscode.lua
@@ -7,35 +7,35 @@ local vscode = require("vscode")
 vim.notify = vscode.notify
 
 wk.add({
-  { "n", "<leader>f",  function() vscode.action("workbench.action.quickOpen") end },
-  { "n", "<leader>r",  function() vscode.action("workbench.action.openRecent") end },
-  { "n", "<leader>,",  function() vscode.action("workbench.action.showAllEditors") end },
-  { "n", "<leader>'",  function() vscode.action("workbench.action.openNextRecentlyUsedEditorInGroup") end },
-  { "n", "<leader>e",  function() vscode.action("workbench.view.explorer") end },
-  { "n", "<leader>bn", function() vscode.action("workbench.action.files.newUntitledFile") end },
+  { "<leader>f",  function() vscode.action("workbench.action.quickOpen") end },
+  { "<leader>r",  function() vscode.action("workbench.action.openRecent") end },
+  { "<leader>,",  function() vscode.action("workbench.action.showAllEditors") end },
+  { "<leader>'",  function() vscode.action("workbench.action.openNextRecentlyUsedEditorInGroup") end },
+  { "<leader>e",  function() vscode.action("workbench.view.explorer") end },
+  { "<leader>bn", function() vscode.action("workbench.action.files.newUntitledFile") end },
 
-  { "n", "<leader>t",  function() vscode.action("workbench.action.editor.changeLanguageMode") end },
+  { "<leader>t",  function() vscode.action("workbench.action.editor.changeLanguageMode") end },
 
-  { "n", "<leader>sk", function() vscode.action("workbench.action.openGlobalKeybindings") end },
+  { "<leader>sk", function() vscode.action("workbench.action.openGlobalKeybindings") end },
 
-  { "n", "]d",         function() vscode.action("editor.action.marker.next") end },
-  { "n", "[d",         function() vscode.action("editor.action.marker.prev") end },
-  { "n", "<leader>sd", function() vscode.action("workbench.actions.view.problems") end },
+  { "]d",         function() vscode.action("editor.action.marker.next") end },
+  { "[d",         function() vscode.action("editor.action.marker.prev") end },
+  { "<leader>sd", function() vscode.action("workbench.actions.view.problems") end },
 
-  { "n", "]h",         function() vscode.action("workbench.action.editor.nextChange") end },     -- next hunk
-  { "n", "[h",         function() vscode.action("workbench.action.editor.previousChange") end }, -- prev hunk
+  { "]h",         function() vscode.action("workbench.action.editor.nextChange") end },     -- next hunk
+  { "[h",         function() vscode.action("workbench.action.editor.previousChange") end }, -- prev hunk
 
-  { "n", "<leader>ui", function() vscode.action("editor.action.inspectTMScopes") end },
-  { "n", "<leader>uC", function() vscode.action("workbench.action.selectTheme") end },
-  { "n", "<a-z>",      function() vscode.action("workbench.action.toggleZenMode") end },
+  { "<leader>ui", function() vscode.action("editor.action.inspectTMScopes") end },
+  { "<leader>uC", function() vscode.action("workbench.action.selectTheme") end },
+  { "<a-z>",      function() vscode.action("workbench.action.toggleZenMode") end },
 
-  { "n", "za",         function() vscode.action("editor.toggleFold") end },
-  { "n", "zc",         function() vscode.action("editor.fold") end },
-  { "n", "zC",         function() vscode.action("editor.foldRecursively") end },
-  { "n", "zo",         function() vscode.action("editor.unfold") end },
-  { "n", "zO",         function() vscode.action("editor.unfoldRecursively") end },
-  { "n", "zM",         function() vscode.action("editor.foldAll") end },
-  { "n", "zR",         function() vscode.action("editor.unfoldAll") end },
+  { "za",         function() vscode.action("editor.toggleFold") end },
+  { "zc",         function() vscode.action("editor.fold") end },
+  { "zC",         function() vscode.action("editor.foldRecursively") end },
+  { "zo",         function() vscode.action("editor.unfold") end },
+  { "zO",         function() vscode.action("editor.unfoldRecursively") end },
+  { "zM",         function() vscode.action("editor.foldAll") end },
+  { "zR",         function() vscode.action("editor.unfoldAll") end },
 
   {
     mode = { "n", "x" },
