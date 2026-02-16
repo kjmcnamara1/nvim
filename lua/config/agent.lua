a72f26a28b58de22ba8a53dd247ce9b734a235b9
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-02-16 16:39:56 -0500

    chore(agent): add optional agents
    grok and ollama

--- lua/config/agent.lua
+++ lua/config/agent.lua
@@ -15,15 +15,23 @@ require("avante").setup({
       -- list models with:
       -- curl -s "https://generativelanguage.googleapis.com/v1beta/models?key=$GEMINI_API_KEY" | jq .models[].name
       -- model = "gemini-3-flash-preview",
-      -- model = "gemini-flash-latest",
-      model = "gemini-2.5-flash",
-      endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+      model = "gemini-flash-latest",
+      -- model = "gemini-2.5-flash",
+      -- model = "gemini-1.5-flash",
+      -- endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
     },
     -- grok = {
     --   __inherited_from = "openai",
-    --   model = "grok-beta",
+    --   -- model = "grok-beta",
+    --   model = "llama-3.3-70b-versatile",
     --   endpoint = "https://api.x.ai/v1",
     --   api_key_name = "XAI_API_KEY",
+    -- },
+    -- ollama = {
+    --   __inherited_from = "openai",
+    --   endpoint = "http://127.0.0.1:11434/v1",
+    --   model = "qwen2.5-coder:7b",
+    --   api_key_name = "OLLAMA_API_KEY",
     -- },
   }
 })
