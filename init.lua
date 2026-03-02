549d5421812a624bdba0f461a07876b177cf0356
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-03-02 18:01:33 -0500

    refactor: files

--- init.lua
+++ init.lua
@@ -43,7 +43,7 @@ require("config.plenary") -- all
 -- require("config.todo")       -- default
 -- require("config.treesitter") -- all
 -- require("config.icons")      -- default? or all
-require("config.files")      -- default
+-- require("config.files")      -- default
 require("config.cursor")     -- all
 -- require("config.comment")    -- all
 require("config.dial")       -- all
