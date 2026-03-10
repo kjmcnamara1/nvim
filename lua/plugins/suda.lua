d372c4b694b8c9e301b97c6e10df67e2149aec0a
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-03-09 23:47:32 -0400

    feat(save): prompt for file name when saving new buffer

--- lua/plugins/suda.lua
+++ lua/plugins/suda.lua
@@ -1,11 +1,29 @@
+local function save_w_prompt(opts)
+  local buf_name = vim.api.nvim_buf_get_name(0)
+  local cmd_prefix = opts and opts.noautocmd and "noautocmd " or ""
+
+  if buf_name == "" then
+    vim.ui.input({ prompt = "Save as: " }, function(input)
+      if not input or input == "" then
+        return
+      end
+      vim.cmd(cmd_prefix .. "write! ++p " .. input)
+    end)
+  else
+    vim.cmd(cmd_prefix .. "write! ++p")
+  end
+end
+
 return {
   "lambdalisue/vim-suda",
   lazy = false,
   cmd = { "SudaRead", "SudaWrite" },
   keys = {
     mode = { "n", "x" },
-    { "<c-s>",   "<cmd>w! ++p<cr>",           desc = "Save file" },
-    { "<c-s-s>", "<cmd>noautocmd w! ++p<cr>", desc = "Save w/o formatting" },
+    { "<c-s>",   function() save_w_prompt() end,                     desc = "Save file" },
+    { "<c-s-s>", function() save_w_prompt({ noautocmd = true }) end, desc = "Save w/o formatting" },
+    -- { "<c-s>",   "<cmd>w! ++p<cr>",           desc = "Save file" },
+    -- { "<c-s-s>", "<cmd>noautocmd w! ++p<cr>", desc = "Save w/o formatting" },
   },
   init = function()
     vim.g.suda_smart_edit = 1
