14d9d3f7672a53fd39403e53fbd917c15104d8a5
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-03-16 17:15:23 -0400

    feat(treesitter): add mise
    add is_mise predicate
    add mise injections for toml files
    add otter.nvim for lsp injection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -60,7 +60,17 @@ return {
     },
     init = function()
       require("nvim-treesitter").install({ "regex" })
-      vim.treesitter.language.register("bash", "PKGBUILD")
+      -- vim.treesitter.language.register("bash", "PKGBUILD")
+
+      -- Prepend mise shims to PATH
+      vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
+
+      -- Add is-mise? predicate
+      require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+        local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+        local filename = vim.fn.fnamemodify(filepath, ":t")
+        return string.match(filename, ".*mise.*%.toml$") ~= nil
+      end, { force = true, all = false })
 
       vim.api.nvim_create_autocmd("FileType", {
         desc = "Auto configure treesitter for each filetype",
