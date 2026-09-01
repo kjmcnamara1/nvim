5c3a0c190043aa0e68dd5c3d79e826c29c7bb0da
Kevin McNamara <kevin.j.mcnamara+tech@proton.me>
2026-09-01 15:55:25 -0400

    fix(neorg): only load neorg in default profile

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-neorg/neorg",
+  cond = profile() == "default",
   lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = "*", -- Pin Neorg to the latest stable release
   opts = {},
